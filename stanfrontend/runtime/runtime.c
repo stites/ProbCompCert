982860825a2a0341d4bf906321eb819cb9a502e3
Jean-Baptiste Tristan <tristanj@bc.edu>
2020-11-13 11:16:40 -0500

    Runtime with example

---
+++ stanfrontend/runtime/runtime.c
@@ -0,0 +1,101 @@
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+
+double logdensity(double parameters[], int p_size);
+int get_parameters_size();
+
+double randn (double mu, double sigma)
+{
+  double U1, U2, W, mult;
+  static double X1, X2;
+  static int call = 0;
+ 
+  if (call == 1)
+    {
+      call = !call;
+      return (mu + sigma * (double) X2);
+    }
+ 
+  do
+    {
+      U1 = -1 + ((double) rand () / RAND_MAX) * 2;
+      U2 = -1 + ((double) rand () / RAND_MAX) * 2;
+      W = pow (U1, 2) + pow (U2, 2);
+    }
+  while (W >= 1 || W == 0);
+ 
+  mult = sqrt ((-2 * log (W)) / W);
+  X1 = U1 * mult;
+  X2 = U2 * mult;
+ 
+  call = !call;
+ 
+  return (mu + sigma * (double) X1);
+}
+
+void runtime(int n, int p_size) {
+
+  double parameters[p_size];
+
+  for (int j = 0; j < p_size; ++j) {
+
+    parameters[j] = 0.5;
+
+  }
+  
+  double candidate[p_size];
+  
+  for (int i = 0; i < n; ++i) {
+
+    for (int j = 0; j < p_size; ++j) {
+
+      candidate[j] = parameters[j] + randn(0,1);
+
+    }
+
+    double lp_parameters = logdensity(parameters,p_size);
+    double lp_candidate = logdensity(candidate,p_size);
+    
+    double u = ((double) rand() / RAND_MAX);
+    
+    if (u <= lp_candidate - lp_parameters) {
+
+      for (int j = 0; j < p_size; ++j) {
+
+	parameters[j] = candidate[j];
+
+      }
+      
+    }
+
+    for (int j = 0; j < p_size; ++j) {
+
+      printf("%f",parameters[j]);
+      
+    }
+
+    printf("\n");
+    
+  }
+
+}
+
+
+
+int main(int argc, char* argv[]) {
+
+  if (argc != 2) {
+
+    printf("One argument required: number of iterations\n");
+    exit(1);
+    
+  }
+
+  int p_size = get_parameters_size();
+  
+  runtime(atoi(argv[1]),p_size);
+
+  return 0;
+  
+}
---
+++ stanfrontend/test/program.c
@@ -0,0 +1,32 @@
+#include <math.h>
+
+int d_size = 10;
+int data[10] = {0,1,1,1,1,1,0,1,1,0}; 
+
+double logdensity(double parameters[], int p_size) {
+
+  double target = 0.0;
+
+  if (parameters[0] < 0 || parameters[0] > 1) {
+
+    return -INFINITY;
+
+  }
+  
+  for (int i = 0; i < d_size; ++i) {
+
+    target += data[i] * log(parameters[0]) + (1 - data[i]) * log (1 - parameters[0]);
+    
+  }
+
+  return target;
+  
+}
+
+int get_parameters_size() {
+
+  return 1;
+
+}
+
+
