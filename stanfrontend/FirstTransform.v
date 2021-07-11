1b941bc8e242a5023f6850cf3d83555a8284b4af
Sam Stites <stites@users.noreply.github.com>
2021-07-10 21:48:16 -0400

    init constraints pass

--- stanfrontend/FirstTransform.v
+++ stanfrontend/FirstTransform.v
@@ -142,9 +142,60 @@ match s with
     ret (Ssequence
           (Scall (Some tmp) d params)
           (Starget etmp))
-
-
 end.
+
+Fixpoint transf_constraints_expr (e: CStan.expr) {struct e}: mon CStan.expr :=
+  match e with
+  | CStan.Econst_int i t => ret (CStan.Econst_int i t)
+  | CStan.Econst_float f t => ret (CStan.Econst_float f t)
+  | CStan.Econst_single f t => ret (CStan.Econst_single f t)
+  | CStan.Econst_long i t => ret (CStan.Econst_long i t)
+  | CStan.Evar i t => ret (CStan.Evar i t)
+  | CStan.Etempvar i t => ret (CStan.Etempvar i t)
+  | CStan.Ederef e t => do e <~ transf_constraints_expr e; ret (CStan.Ederef e t)
+  | CStan.Eunop uop e t => do e <~ transf_constraints_expr e; ret (CStan.Eunop uop e t)
+  | CStan.Ebinop bop e0 e1 t =>
+    do e0 <~ transf_constraints_expr e0;
+    do e1 <~ transf_constraints_expr e1;
+    ret (CStan.Ebinop bop e0 e1 t)
+  | CStan.Esizeof t0 t1 => ret (CStan.Esizeof t0 t1)
+  | CStan.Ealignof t0 t1 => ret (CStan.Ealignof t0 t1)
+  | CStan.Etarget ty => ret (CStan.Etarget ty)
+end.
+
+Fixpoint transf_constraints_statement (s: CStan.statement) {struct s}: mon CStan.statement :=
+match s with
+  | Sassign e0 e1 =>
+    do e0 <~ transf_constraints_expr e0;
+    do e1 <~ transf_constraints_expr e1;
+    ret (Sassign e0 e1)
+  | Sset i e => do e <~ transf_constraints_expr e; ret (Sset i e)
+  | Scall oi e le =>
+    do e <~ transf_constraints_expr e;
+    do le <~ mon_mmap (transf_constraints_expr) le;
+    ret (Scall oi e le)
+  | Sbuiltin oi ef lt le => error (msg "ret (Sbuiltin oi ef lt le)")
+  | Ssequence s0 s1 =>
+    do s0 <~ transf_constraints_statement s0;
+    do s1 <~ transf_constraints_statement s1;
+    ret (Ssequence s0 s1)
+  | Sifthenelse e s0 s1 =>
+    do e <~ transf_constraints_expr e;
+    do s0 <~ transf_constraints_statement s0;
+    do s1 <~ transf_constraints_statement s1;
+    ret (Sifthenelse e s0 s1)
+  | Sloop s0 s1 =>
+    do s0 <~ transf_constraints_statement s0;
+    do s1 <~ transf_constraints_statement s1;
+    ret (Sloop s0 s1)
+  | Sreturn oe => do oe <~ option_mon_mmap (transf_constraints_expr) oe; ret (Sreturn oe)
+  | Starget e => error (msg "Starget DNE in this stage of pipeline")
+  | Stilde e i le (oe0, oe1) => error (msg "Stilde DNE in this stage of pipeline")
+  | Sbreak => ret Sbreak
+  | Sskip => ret Sskip
+  | Scontinue => ret Scontinue
+end.
+
 
 Notation localvar := (prod AST.ident Ctypes.type).
 
@@ -230,9 +281,10 @@ Definition transf_model (p:program) (f: function) (body : statement): mon statem
   end.
 
 Definition transf_statement_pipeline (p:program) (f: function) : mon CStan.statement :=
-  do body <~ transf_statement f.(fn_body); (* Stilde -> Starget; Error "Backend: tilde" *)
-  do body <~ transf_statement body;        (* apply Starget transform *)
-  do body <~ transf_model p f body;
+  do body <~ transf_statement f.(fn_body);          (* Stilde -> Starget; Error "Backend: tilde" *)
+  do body <~ transf_statement body;                 (* apply Starget transform *)
+  do body <~ transf_constraints_statement body;     (* apply constraint transformations *)
+  do body <~ transf_model p f body;                 (* add target preamble and replace Etarget with Evar target_ident *)
   ret body.
 
 Definition transf_function (p:CStan.program) (f: function): res function :=
