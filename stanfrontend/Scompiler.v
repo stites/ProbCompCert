588497a99c2a0f383c4c179803b8f8f6108bd1e3
Sam Stites <stites@users.noreply.github.com>
2021-07-20 21:52:27 -0400

    Revert "move compilation of target ident to EmbedTarget"
    This reverts commit 6a26af132fb5062e99b536be4657a5f4127609c4.

--- stanfrontend/Scompiler.v
+++ stanfrontend/Scompiler.v
@@ -8,16 +8,14 @@ Require Import Stemplate.
 Require Import Compiler.
 Require Import Denumpyification.
 Require Import FirstTransform.
-Require Import EmbedTarget.
 Require Import String.
 Require Import Sbackend.
-Open Scope string_scope.
+Open Scope string_scope.						     
 
 Definition transf_stan_program(p: StanE.program): res Clight.program :=
   OK p
   @@@ time "Denumpyification" Denumpyification.transf_program
   @@@ time "FirstTransform" FirstTransform.transf_program
-  @@@ time "EmbedTarget" EmbedTarget.transf_program
   @@@ time "Backend" backend.
   
 Theorem transf_stan_program_correct:
