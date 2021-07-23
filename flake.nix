48cbca4c42aea9296a25847770f157ebcd3b98ea
Sam Stites <stites@users.noreply.github.com>
2021-07-23 10:17:11 -0400

    can't overload reserved main function

--- flake.nix
+++ flake.nix
@@ -73,20 +73,20 @@
           in [
             (mk-watcher "watch-stan" {
               command = watch {
-                build = "make -j && make install";
+                build = "make -j && make install && rm ccomp clightgen";
                 cmd = "./out/bin/ccomp -c $current_dir/$1";
               };
             })
             (mk-watcher "watch-stan-debug" {
               command = watch {
-                build = "make -j && make install";
+                build = "make -j && make install && rm ccomp clightgen";
                 cmd = "./out/bin/ccomp -c $current_dir/$1";
                 finally = "./out/bin/clightgen $current_dir/$1";
               };
             })
             (mk-watcher "watch-clightgen" {
               command = watch {
-                build = "make -j && make install";
+                build = "make -j && make install && rm ccomp clightgen";
                 cmd = "./clightgen $current_dir/$1";
               };
             })
