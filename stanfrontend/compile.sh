7175c30f76bd4475d75d962c4050cbb4a884805a
Jean-Baptiste Tristan <tristanj@bc.edu>
2022-01-10 15:44:39 -0500

    Better compilation script and classic examples

--- stanfrontend/compile.sh
+++ stanfrontend/compile.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-../ccomp -c tests/simple/simple.stan
+prog=${1##*/}
+name=${prog%.*}
+
+echo $prog
+echo $name
+
+../ccomp -c $1
 ../ccomp -c stanlib.c
 ../ccomp -c Runtime.c
-../ccomp stanlib.o Runtime.o simple.s -o simple
+../ccomp stanlib.o Runtime.o $name.s -o $name
 
